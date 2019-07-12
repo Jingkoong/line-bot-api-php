8790590e5d27a011e73f6e256cd3612924acc7c3
Phicha Kaeochitkongthong <theolanter@gmail.com>
2019-07-12 14:27:46 +0700

    EDIT APPID

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "OCbJNzksoFmEdXC"; //enter your web key
-		$SECRET = "UwTwehyr7ikZnXcJv7acvwTQq"; //enter your secret
+		$APPID= "lineBot/"; //enter your appid
+		$KEY = "G6uP8zjV7lKlcEQ"; //enter your web key
+		$SECRET = "yzL3ZkHVp7Eyi9Ll7hGeqcVn6"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
